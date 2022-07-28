5cba049bd2cf52bee04ba01648dc8885d03f4bc4
Agustin Rojas <agus.rojasmolina@gmail.com>
2022-07-28 03:34:28 -0400

    cambios en el script

--- js/fotoTienda.js
+++ js/fotoTienda.js
@@ -1,6 +1,5 @@
 // En este script está el código referente a la seccion tienda(mi e-commerce) del sitio.
 
-
 'use strict'
 
 let fotoProductos = [
@@ -8,7 +7,7 @@ let fotoProductos = [
         id:"001",
         nombre:'Chinita',
         tamaño:'30x40cm, 40x30cm',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print1.jpg',
         descripcion:'',
@@ -17,7 +16,7 @@ let fotoProductos = [
         id:"002",
         nombre:'2 Psillobora picta',
         tamaño:'30x40cm, 40x30cm',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print2.jpg',
         descripcion:''
@@ -26,7 +25,7 @@ let fotoProductos = [
         id:"003",
         nombre:'gorgojo de la rosa',
         tamaño:'30x40cm, 40x30cm',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print3.jpg',
         descripcion:''
@@ -35,7 +34,7 @@ let fotoProductos = [
         id:"004",
         nombre:'mariposa de la tarde',
         tamaño:'30x40cm, 40x30cm',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print4.jpg',
         descripcion:''
@@ -44,7 +43,7 @@ let fotoProductos = [
         id:"005",
         nombre:'Chinche',
         tamaño:'30x40cm, 40x30cm',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print5.jpg',
         descripcion:''
@@ -53,7 +52,7 @@ let fotoProductos = [
         id:"006",
         nombre:'Salticido y mosca',
         tamaño:'30x40cm, 40x30cm',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print6.jpg',
         descripcion:''
@@ -62,7 +61,7 @@ let fotoProductos = [
         id:"007",
         nombre:'Hormigon dorado alado',
         tamaño:'30x40cm, 40x30cm, 40',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print7.jpg',
         descripcion:''
@@ -71,7 +70,7 @@ let fotoProductos = [
         id:"008",
         nombre:'Chanchito blanco',
         tamaño:'30x40cm, 40x30cm, 40',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print8.jpg',
         descripcion:''
@@ -81,7 +80,7 @@ let fotoProductos = [
         id:"009",
         nombre:'Xylocopa Augusti',
         tamaño:'30x40cm, 40x30cm',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print9.jpg',
         descripcion:''
@@ -90,7 +89,7 @@ let fotoProductos = [
         id:"010",
         nombre:'Ariadna maxima',
         tamaño:'30x40cm, 40x30cm',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/printt10.jpg',
         descripcion:''
@@ -99,7 +98,7 @@ let fotoProductos = [
         id:"011",
         nombre:'Coniopterígido',
         tamaño:'30x40cm, 40x30cm',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print11.jpg',
         descripcion:''
@@ -108,50 +107,27 @@ let fotoProductos = [
         id:"012",
         nombre:'Abeja joya',
         tamaño:'30x40cm, 40x30cm, 40',
-        precio:'20.000',
+        precio:'20000',
         stock:'',
         imagen:'../images/print12.jpg',
         descripcion:''
     },
 
 ]
-/*
-let listaProductos = document.querySelector('#cards')
 
-for (let producto of fotoProductos){
-    listaProductos.innerHTML += `
-        <div class="cards cards--$">
-            <div>
-                <img src="${producto.imagen}" class="fotos--tienda">
-            </div>
-            <div>
-                <h3>
-                ${producto.nombre}
-                </h3>
-                <p>${producto.tamaño}</p>
-            </div>
-            <div>
-                <p>$${producto.precio}</p>
-            </div>
-        </div>
-    `
-}
-*/
 
 let carrito
 let cardCart
-const contador = document.querySelector('#contador--producto')
 
+// ADD KEY CARRITO TO LOCAL STORAGE
 if(JSON.parse(localStorage.getItem('carrito'))) { // si existe la clave carrito en el storage
     carrito = JSON.parse(localStorage.getItem('carrito')) // asignar dicho valor en la variable carrito
 } else { // si no existe
     localStorage.setItem('carrito', JSON.stringify([])) // seteado el local storage
     carrito = JSON.parse(localStorage.getItem('carrito')) // a la variable carrito quiero que sea lo que setié en el localStorage
 }
-contador.innerHTML = `
-    <p>${carrito.length}</p>
-    `
 
+//PUT CARDS OF PRODUCTS INTO HTML
 function mostrarProductos() {
     for (let i = 0; i < fotoProductos.length; i++) {
         const element = fotoProductos[i]
@@ -179,7 +155,7 @@ function mostrarProductos() {
             </div>
             
             <div class="btn--container">
-                <button id=${id} class="btn--agregar">Agregar al carrito</button>
+                <button id=${id} class="btn--agregar" type="button">Agregar al carrito</button>
             </div>
         </div>
         `
@@ -188,21 +164,29 @@ function mostrarProductos() {
     }
 }
 mostrarProductos()
- // id para div del contador en el .html
+
 
 const btnAgregar = document.getElementsByClassName('btn--agregar')
-console.log(btnAgregar)
+let btnQuitar = document.getElementsByClassName('btn--quitar--producto')
+ // CART ITEMS COUNTER
+const contadorProductos = document.querySelector('#contador--producto')
+let contador = 0
+contadorProductos.innerHTML = `
+    <p>${contador}</p>
+    `
+
+// ADDEVENTLISTENER A CADA BTN--AGREGAR
 for (let i = 0; i < btnAgregar.length; i++) {
     const element = btnAgregar[i]
     element.addEventListener('click', agregarAlCarrito)
-} 
+}
 
 function agregarAlCarrito(e){
     const btn = e.target // trae exactamente el boton que el usuario clickea.
     const idBoton = btn.getAttribute('id') // sacar atributos de las etiquetas. trae el id del boton que clickea el usuario
     const productoEncontrado = fotoProductos.find((fotoProd) => fotoProd.id == idBoton) // find para saber que producto le corresponde el id, osea que fotoProd.id sea igual a lo que dice idBoton
     const enCarrito = carrito.find((fotoProd) => fotoProd.id == productoEncontrado.id) // al producto encontrado es igual al id de alguno de los productos? //recorre los productos que tenga en el carrito
-    console.log(carrito)
+
     if(!enCarrito) { // si no existe el producto en el carrito entonces hacer esto: agregar el producto y la propiedad cantidad: 1 (propiedad cantidad añadida acá)
         carrito.push({...productoEncontrado, cantidad: 1}) //pushear al carrito. El "..." es para que en vez de pegar el objeto entero(porque productoEncontrado es un objeto), pega propiedad por propiedad. 
     } else { // si ya existe, entonces hacer lo siguiente:
@@ -210,12 +194,59 @@ function agregarAlCarrito(e){
         carrito = [...carritoFiltrado, {...enCarrito, cantidad: enCarrito.cantidad + 1}] //agregar todos los productos(propiedad por propiedad) menos el que yo encontré en el carrito. agregar la cantidad que yo tenía en el carrito del producto + 1
     }
     localStorage.setItem('carrito', JSON.stringify(carrito))
-    
-    
-    contador.innerHTML = `
-    <p>${carrito.length}</p>
+    //
+    contador++
+    contadorProductos.innerHTML = `
+    <p>${contador}</p>
     `
+    console.log(carrito)
 }
+
+
+// REMOVE ITEMS (not implemented yet :D)
+/*
+
+for (let i = 0; i < btnAgregar.length; i++) {
+    const element = btnAgregar[i]
+    element.addEventListener('click', quitarDelCarrito)
+}
+function quitarDelCarrito(e){
+    const btn = e.target
+    const idBoton = btn.getAttribute('id')
+    //document.getElementById(`idBoton`).remove()
+    carrito.splice(id,1)
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+//esta no funcionó
+for (let i = 0; i < btnQuitar.length; i++){
+    const element = btnQuitar[i]
+    element.addEventListener('click', () => {
+        let carritoStorage = JSON.parse(localStorage.getItem('carrito'))
+        carritoStorage.forEach((producto, id) => {
+            let botonCard = document.getElementById(`${id}`)
+            botonCard.addEventListener('click', () => {
+                document.getElementById(`${id}`).remove()
+                carrito.splice(id,1)
+                localStorage.setItem('carrito', JSON.stringify(carrito))
+                console.log(`${producto.nombre} Eliminada`)
+            })
+        })
+    })
+}
+
+// ahora pruebo de otra forma
+for (let i = 0; i < btnAgregar.length; i++) {
+    const element = btnAgregar[i]
+    element.addEventListener('click', quitarDelCarrito)
+}
+function quitarDelCarrito(e){
+    const btn = e.target
+    const idBoton = btn.getAttribute('id')
+    document.getElementById(`idBoton`).remove()
+    carrito.splice(id,1)
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+*/
 
 
 // mostrar/ocultar carrito/tienda
@@ -224,6 +255,7 @@ const verTienda = document.querySelector('#ver--tienda')
 const divCarrito = document.querySelector('#div--carrito')
 const fotoTienda = document.querySelector('#foto--tienda')
 const divCarritoProductos = document.querySelector('#div--carrito--productos')
+const divTotalProductos = document.querySelector('#total--carrito')
 
 let validadorCarrito = true
 verCarrito.addEventListener('click', function(){
@@ -231,12 +263,17 @@ verCarrito.addEventListener('click', function(){
     divCarrito.style.display = 'flex'
     verCarrito.style.display = 'none'
     verTienda.style.display = 'block'
+    divTotalProductos.style.display = 'flex'
     if(validadorCarrito) {
         mostrarCarrito()
+        console.log(carrito)
         validadorCarrito = false
+        
     } else {
         const cardEliminar = ``
+        const totalEliminar = ``
         divCarritoProductos.innerHTML = cardEliminar
+        divTotalProductos.innerHTML = totalEliminar
         mostrarCarrito()
     }
 })
@@ -246,18 +283,17 @@ verTienda.addEventListener('click', function(){
     divCarrito.style.display = 'none'
     verTienda.style.display = 'none'
     verCarrito.style.display = 'block'
+    divTotalProductos.style.display = 'none'
 })
 
 
-// Carrito 
-
-const totalCarrito = () => { // funcion anonima para que de el total del carrito
+// TotalCarrito 
+function totalCarrito(){ // funcion para que de el total del carrito
     return carrito.reduce((acumulador, fotoProd) => acumulador + fotoProd.precio * fotoProd.cantidad, 0) //recurrer el producto. acumulador + el precio del producto multiplicado por la cantidad (la cantidad propiedad ya va a estar creada)
 }
 
-
 // cambio a página carrito de compras. 2 páginas una cuando no existan productos en el carrito y la otra cuando si existan
-
+// SHOW CART
 function mostrarCarrito(){
     if(carrito.length == 0) { 
         const textoCarritoVacio = `
@@ -284,12 +320,29 @@ function mostrarCarrito(){
                     <p>${cantidad}</p>
                 </div>
                 <div>
-                    <p>$${precio}</p>
+                    <p>$${(precio * cantidad).toLocaleString()}</p>
+                </div>
+                <div class="quitar--producto">
+                    <button id=${id} class="btn--quitar--producto">Quitar producto</button>
                 </div>
             </div>
             ` 
             divCarritoProductos.innerHTML += cardCart
-            
+
         }
+        const cardTotal = `
+            <div>
+                <h2>
+                    Total:
+                </h2>
+            </div>
+            <div>
+                <h2>
+                    $${totalCarrito().toLocaleString()}
+                </h2>
+            </div>
+            ` 
+        divTotalProductos.innerHTML += cardTotal
+        
     }
 }
